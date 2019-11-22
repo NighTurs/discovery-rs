3ebf6bb3fd1adb7b0d5203c57b74b171cd428e32
NighTurs <nighturs@gmail.com>
2019-11-22 06:04:38 +0300

    Follow style guides in indexer

--- scripts/indexer.js
+++ scripts/indexer.js
@@ -3,27 +3,26 @@ const csv = require('csv-parser');
 const MiniSearch = require('minisearch');
 
 let index = null;
-let rows = [];
+const rows = [];
 
-fs.createReadStream(process.argv[2] + '/web.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        if (!index) {
-            let fields = [];
-            for (field in row) {
-                if (field.startsWith('t_')) {
-                    fields.push(field);
-                }
-            }
-            fields.push('t_flags');
-            index = new MiniSearch({fields: fields, idField: 'idx'});
+fs.createReadStream(`${process.argv[2]}/web.csv`)
+  .pipe(csv())
+  .on('data', (row) => {
+    if (!index) {
+      const fields = [];
+      Object.keys(row).forEach((field) => {
+        if (field.startsWith('t_')) {
+          fields.push(field);
         }
-        row.idx = +row.idx;
-        rows.push(row);
-    })
-    .on('end', () => {
-        index.addAll(rows);
-        fs.writeFileSync(process.argv[2] + '/web_index.json', JSON.stringify(index.toJSON()));
-    });
-
-
+      });
+      fields.push('t_flags');
+      index = new MiniSearch({ fields, idField: 'idx' });
+    }
+    // eslint-disable-next-line no-param-reassign
+    row.idx = +row.idx;
+    rows.push(row);
+  })
+  .on('end', () => {
+    index.addAll(rows);
+    fs.writeFileSync(`${process.argv[2]}/web_index.json`, JSON.stringify(index.toJSON()));
+  });
