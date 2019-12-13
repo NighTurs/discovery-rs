138691c85a95ad8a17e500351242102a87ea0df0
NighTurs <nighturs@gmail.com>
2019-12-14 02:23:53 +0300

    Github pages deploy script

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,14 @@
+if git show-ref --verify --quiet "refs/heads/gh-pages"; then
+    git push -d origin gh-pages
+    git branch -D gh-pages
+fi
+
+git --work-tree web checkout --orphan gh-pages
+git --work-tree web add --all
+git --work-tree web rm -r --cached data
+git --work-tree web add data/ml.zip
+git --work-tree web add data/lastfm.zip
+git --work-tree web add data/gbook.zip
+git --work-tree web commit -m "Github pages deploy"
+git push origin gh-pages
+git checkout master --force
